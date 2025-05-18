c5e0ecf730a3a4a4159c08b8b85524e561aceccb
Riyaz <sr308379@gmail.com>
2025-05-18 22:32:06 +0530

    feat: update axios base URL to production environment

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:9876', // common base URL
+  baseURL: 'https://recruitment-bot-vercel.onrender.com', // common base URL
   headers: {
     'Content-Type': 'application/json',
   },
