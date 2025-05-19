dd3ad914e161a889a5f6525d01d609a1284a085e
Riyaz <sr308379@gmail.com>
2025-05-19 10:43:47 +0530

    feat: update cookie sameSite attribute to 'none' for cross-site requests

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -71,7 +71,7 @@ const googleAuth = async (req, res) => {
       res.cookie("token", token, {
         httpOnly: true,
         secure: false, 
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000, 
       });
 
